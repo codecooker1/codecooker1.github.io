7de79992a3fbc64b75a0bc22542fb19348b141ec
Codecooker Mintz <mcodecooker@gmail.com>
2025-01-23 17:04:50 +0600

    fix

---
+++ assets/AboutView-BCFBd6ij.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-B6l6B_xM.js";const r={};function t(c,_){return"HELLO"}const o=e(r,[["render",t]]);export{o as default};
---
+++ assets/rtView-y6dFLvbH.js
@@ -0,0 +1,1 @@
+import{x as e,o,j as r,q as t,R as a}from"./index-B6l6B_xM.js";const p=e({__name:"rtView",setup(n){return(s,c)=>(o(),r(t(a)))}});export{p as default};
